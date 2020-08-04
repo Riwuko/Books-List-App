5332d637af313b23d282b0b9daf41d3f1dbdd3a9
riwuko x <riwuko@gmail.com>
2020-08-04 18:35:45 +0200

    book model updated and registered

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class Book(models.Model):
-    id = models.CharField(primary_key=True)
-    title = models.CharField()
-    authors = ArrayField(models.CharField()
+    id = models.CharField(max_length=200,primary_key=True)
+    title = models.CharField(max_length=200)
+    authors = ArrayField(models.CharField(max_length=200))
     published_date = models.DateField()
-    categories = ArrayField(models.CharField()
+    categories = ArrayField(models.CharField(max_length=200,blank=True))
     average_rating = models.IntegerField()
-    rating_count = models.FloatField()
+    rating_count = models.IntegerField()
     thumbnail = models.URLField()
 
     def __str__(self):
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -122,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
