494d0419b69878c58c4038720c8fff50762302f8
riwuko x <riwuko@gmail.com>
2020-08-04 16:28:58 +0200

    book model created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    id = models.CharField(primary_key=True)
+    title = models.CharField()
+    authors = ArrayField(models.CharField()
+    published_date = models.DateField()
+    categories = ArrayField(models.CharField()
+    average_rating = models.IntegerField()
+    rating_count = models.FloatField()
+    thumbnail = models.URLField()
+
+    def __str__(self):
+        return f'title: {self.title}, authors: {self.authors}'
