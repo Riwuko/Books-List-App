97a71904e06a0f547c6d734aa3df694cd02fe506
riwuko x <riwuko@gmail.com>
2020-08-06 19:59:37 +0200

    downloading books finished

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path,include
-from . import views
+from app.views import BookViewSet, bookCreateView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('book-list', views.BookViewSet)
+router.register('book-list', BookViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
-    path('book-create',views.bookCreateView,name='book-create')
+    path('book-add',bookCreateView,name='book-add')
+    
 ]
 
 # urlpatterns = [
--- app/views.py
+++ app/views.py
@@ -8,6 +8,8 @@ from rest_framework.pagination import PageNumberPagination
 from .models import Book
 from .serializers import BookSerializer
 
+from app.views_api import download_items, prepare_items
+
 
 class BookViewSet(ReadOnlyModelViewSet):
     queryset = Book.objects.all()
@@ -15,10 +17,20 @@ class BookViewSet(ReadOnlyModelViewSet):
     pagination_class = PageNumberPagination
 
 @api_view(['POST'])
-def bookCreateView(request,id):
-    serializer = BookSerializer(request.data)
+def bookCreateView(request):
+    items = download_items(request.data)
+    prepare_items(items)
+    html = "<html><body>It is now.</body></html>"
+    return Response(html)
 
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
+#     @api_view(['POST'])
+# def bookCreateView(request):
+#     serializer = BookPostSerializer(data=request.data)
+#     print(request.data)
+#     if serializer.is_valid():
+#         print(serializer.data['field'])
+#         download_books(serializer.data['field'])
+#     else:
+#         print(serializer.errors)
+#     html = "<html><body>It is now.</body></html>"
+#     return Response(html)
