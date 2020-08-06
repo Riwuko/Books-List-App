824de6b29007b78bb1a2435e534b4140795b248e
riwuko x <riwuko@gmail.com>
2020-08-06 14:37:45 +0200

    id type changed

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 class Book(models.Model):
-    id = models.CharField(max_length=200,primary_key=True)
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     authors = ArrayField(models.CharField(max_length=200))
     published_date = models.DateField()
