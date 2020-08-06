07e9dcc712ddf44bad4f1663ad31def093d326ac
riwuko x <riwuko@gmail.com>
2020-08-07 00:16:02 +0200

    id removed from model

--- app/models.py
+++ app/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 class Book(models.Model):
-    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     authors = ArrayField(models.CharField(max_length=200))
     published_date = models.DateField()
-    categories = ArrayField(models.CharField(max_length=200,blank=True))
-    average_rating = models.FloatField()
-    rating_count = models.FloatField()
-    thumbnail = models.URLField()
+    categories = ArrayField(models.CharField(max_length=200,blank=True, null=True))
+    average_rating = models.FloatField(blank=True, null=True)
+    rating_count = models.FloatField(blank=True, null=True)
+    thumbnail = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return f'title: {self.title}, authors: {self.authors}'
