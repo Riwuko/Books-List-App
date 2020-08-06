cec05ad2294a613ea5d6c2dab47f44b44f5a16a0
riwuko x <riwuko@gmail.com>
2020-08-06 22:39:17 +0200

    filtering and ordering system added

--- config/settings.py
+++ config/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
 
     'app',
     'rest_framework',
+    'django_filters',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination', 
     'PAGE_SIZE':1,
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend','rest_framework.filters.OrderingFilter',),
 }
 
 MIDDLEWARE = [
