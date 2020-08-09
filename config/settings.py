4c3b8c2b936689631eeb157aa0204b70ce78a18c
riwuko x <riwuko@gmail.com>
2020-08-09 17:58:21 +0200

    another changes in heroku settings

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import environ
 import django_heroku
+import dj_database_url
 
 env = environ.Env()
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
