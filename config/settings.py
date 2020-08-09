d4244913036cde12c784050e1af6eb560c225aeb
riwuko x <riwuko@gmail.com>
2020-08-09 18:11:33 +0200

    old settings and directories removed

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import environ
-import django_heroku
 import dj_database_url
 
 env = environ.Env()
@@ -143,5 +142,3 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
