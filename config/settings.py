c8e6378635d7fa77d40f297efd7a3ddaf0ed8b81
riwuko x <riwuko@gmail.com>
2020-08-09 18:05:30 +0200

    more heroku settings changes

--- config/settings.py
+++ config/settings.py
@@ -17,8 +17,6 @@ import dj_database_url
 
 env = environ.Env()
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -102,6 +100,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
