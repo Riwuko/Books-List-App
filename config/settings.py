d9b40eb2d3c50f08a437bf26225cce2ac3e268d0
riwuko x <riwuko@gmail.com>
2020-08-09 15:01:44 +0200

    example env file added and requirements updated

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
